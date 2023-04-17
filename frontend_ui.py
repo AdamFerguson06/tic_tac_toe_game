273b6c8b0dd80cecb3f3d29aec6b80ec1022876c
Adam Ferguson <adam.ferguson06@gmail.com>
2023-04-17 09:25:35 -0700

    Adding code for backend function and frontend display

--- frontend_ui.py
+++ frontend_ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 from tic_tac_toe import TicTacToe
 
 class TicTacToeGUI(tk.Tk):
@@ -6,18 +7,42 @@ class TicTacToeGUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Tic Tac Toe")
-        self.geometry("300x300")
+        self.geometry("400x250")
         self.tic_tac_toe = TicTacToe()
         self.create_widgets()
 
     def create_widgets(self):
-        pass
+        self.buttons = [[None for _ in range(3)] for _ in range(3)]
+        for row in range(3):
+            for col in range(3):
+                self.buttons[row][col] = tk.Button(self, text=' ', width=10, height=3,
+                                                   command=lambda r=row, c=col: self.on_button_click(r, c))
+                self.buttons[row][col].grid(row=row, column=col)
+
+        self.reset_button = tk.Button(self, text='Reset', command=self.reset_game)
+        self.reset_button.grid(row=3, columnspan=3)
 
     def on_button_click(self, row, col):
-        pass
+        if self.tic_tac_toe.make_move(row, col):
+            self.buttons[row][col].config(text=self.tic_tac_toe.current_player)
+            if self.tic_tac_toe.check_winner():
+                self.display_result(f"Player {self.tic_tac_toe.current_player} wins!")
+            elif self.tic_tac_toe.is_full():
+                self.display_result("It's a draw!")
+            else:
+                self.tic_tac_toe.switch_player()
+        else:
+            tk.messagebox.showerror("Invalid move", "Cell is already occupied, choose another one.")
+
+    def display_result(self, message):
+        tk.messagebox.showinfo("Game Over", message)
+        self.reset_game()
 
     def reset_game(self):
-        pass
+        self.tic_tac_toe = TicTacToe()
+        for row in range(3):
+            for col in range(3):
+                self.buttons[row][col].config(text=' ')
 
 if __name__ == "__main__":
     app = TicTacToeGUI()
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,19 +5,59 @@ class TicTacToe:
         self.current_player = 'X'
 
     def display_board(self):
-        pass
+        for row in self.board:
+            print("|".join(row))
+            print("-" * 5)
 
     def make_move(self, row, col):
-        pass
+        if self.board[row][col] == ' ':
+            self.board[row][col] = self.current_player
+            return True
+        return False
 
     def check_winner(self):
-        pass
+        for row in self.board:
+            if row[0] == row[1] == row[2] != ' ':
+                return True
+
+        for col in range(3):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col] != ' ':
+                return True
+
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
+            return True
+
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
+            return True
+
+        return False
 
     def is_full(self):
-        pass
+        for row in self.board:
+            for cell in row:
+                if cell == ' ':
+                    return False
+        return True
 
     def switch_player(self):
-        pass
+        self.current_player = 'O' if self.current_player == 'X' else 'X'
 
     def play(self):
-        pass
+        while not self.check_winner() and not self.is_full():
+            self.display_board()
+            row, col = map(int, input(f"Player {self.current_player}'s turn. Enter row and column (0-2) separated by space: ").split())
+            if self.make_move(row, col):
+                if self.check_winner():
+                    self.display_board()
+                    print(f"Player {self.current_player} wins!")
+                elif self.is_full():
+                    self.display_board()
+                    print("It's a draw!")
+                else:
+                    self.switch_player()
+            else:
+                print("Invalid move, try again.")
+
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.play()
